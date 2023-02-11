026aefc80b0d3d4d77a7b6e8f3ac9c995a56b9a4
Ahmet timur <ahmettimur9@gmail.com>
2023-02-11 18:37:47 +0300

    fix: added links to footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -46,24 +46,29 @@ const Footer = () => {
         </div>
         <div className="flex flex-col">
           <h1 className="mb-5 text-xl font-bold">İletişim</h1>
-          <div className="flex gap-2">
+          <a href="tel:+905323993916" className="flex gap-2">
             <BsFillTelephoneFill />
             <p>0532 399 39 16</p>
-          </div>
-          <div className="flex gap-2">
+          </a>
+          <button
+            onClick={() => {
+              window.location.href = "mailto:turfilo08@gmail.com}";
+            }}
+            className="flex gap-2"
+          >
             <SiGmail />
-            <p>Turfilo08@gmail.com</p>
-          </div>
+            <p>turfilo08@gmail.com</p>
+          </button>
           <div className="my-3 flex gap-3">
             <a
               className="transition-all hover:text-[rgba(255,255,255,0.55)]"
-              href=""
+              href="https://m.facebook.com/TURFILO/"
             >
               <BsFacebook size={30} />
             </a>
             <a
               className="transition-all hover:text-[rgba(255,255,255,0.55)]"
-              href=""
+              href="https://instagram.com/tur_filo?igshid=YmMyMTA2M2Y="
             >
               <BsInstagram size={30} />
             </a>
